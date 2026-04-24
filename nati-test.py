77c3b77ec2513e1eea54921aadc038e936018a4e
Natalia <natalia.schlebinger@gmail.com>
2026-04-24 11:14:21 -0300

    Otra manera de testear

---
+++ nati-test.py
@@ -0,0 +1,18 @@
+import os
+
+os.system("mkdir /tmp/entradas")
+os.system("mkdir /tmp/salidas")
+
+def testear(n_programa):
+    for i in range(5):
+        os.system(f"cp entradas/factura{i+1}.pdf /tmp/entradas/factura{i+1}.pdf")
+        os.system(f"cp salidas/programa{n_programa}_{i+1}.txt /tmp/salidas/programa{n_programa}_{i+1}.txt")
+
+        os.system(f"python3 programas/programa{n_programa}.py entradas/factura{i+1}.pdf salidas/programa{n_programa}_{i+1}.txt")
+        os.system(f"diff --strip-trailing-cr salidas/programa{n_programa}_{i+1}.txt salidas_esperadas/programa{n_programa}_{i+1}.txt")
+
+        os.system(f"cp /tmp/entradas/factura{i+1}.pdf entradas/factura{i+1}.pdf")
+        os.system(f"cp /tmp/salidas/programa{n_programa}_{i+1}.txt salidas/programa{n_programa}_{i+1}.txt")
+
+
+testear(1)
