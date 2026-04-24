6155ed484d9efe8671d2102906f6b8b720cf2bb1
Natalia <natalia.schlebinger@gmail.com>
2026-04-24 12:23:21 -0300

    Mejoras a mis tests

--- nati-test.py
+++ nati-test.py
@@ -1,28 +1,25 @@
 import os
 
-os.system("mkdir /tmp/entradas")
-os.system("mkdir /tmp/salidas")
-
 def testear(n_programa):
     print(f"TESTANDO PROGRAMA {n_programa}")
+
+    os.system("cp -r entradas /tmp")
+    os.system("cp -r salidas /tmp")
 
     for i in range(5):
         print("="*10)
 
-        os.system(f"cp entradas/factura{i+1}.pdf /tmp/entradas/factura{i+1}.pdf")
-        os.system(f"cp salidas/programa{n_programa}_{i+1}.txt /tmp/salidas/programa{n_programa}_{i+1}.txt")
-
         os.system(f"python3 programas/programa{n_programa}.py entradas/factura{i+1}.pdf salidas/programa{n_programa}_{i+1}.txt")
         os.system(f"diff --strip-trailing-cr salidas/programa{n_programa}_{i+1}.txt salidas_esperadas/programa{n_programa}_{i+1}.txt")
 
-        os.system(f"cp /tmp/entradas/factura{i+1}.pdf entradas/factura{i+1}.pdf")
-        os.system(f"cp /tmp/salidas/programa{n_programa}_{i+1}.txt salidas/programa{n_programa}_{i+1}.txt")
-
         print("="*10)
+    
+    os.system("rm -r entradas")
+    os.system("rm -r salidas")
+    os.system("mv /tmp/entradas entradas")
+    os.system("mv /tmp/salidas salidas")
 
 
 testear(1)
 testear(2)
 
-os.system("rm -r /tmp/entradas")
-os.system("rm -r /tmp/salidas")
